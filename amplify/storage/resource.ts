dc3d409b9a919a435765d3b9c26a7dc2756c6be0
Branislav Cepilek <branislav.cepilek@gmail.com>
2024-06-19 15:06:38 +0200

    allow actions on resources

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,12 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-    name: 'amplifyUniledStorage'
+    name: 'amplifyUniledStorage',
+    access: (allow) => ({
+        'uniled-katalog/*': [
+            allow.guest.to(['get']),
+            allow.authenticated.to(['get', 'write', 'delete']),
+            allow.entity('identity').to(['get', 'write', 'delete'])
+        ]
+    })
 });
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { Authenticator } from "@aws-amplify/ui-vue";
-import "@aws-amplify/ui-vue/styles.css"
+import "@aws-amplify/ui-vue/styles.css";
 
-import Todos from './components/Todos.vue'
+import Todos from './components/Todos.vue';
+
 </script>
 
 <template>
