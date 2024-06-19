7b8d61182123fbd6b078266da616efa2ee854f2c
Branislav Cepilek <branislav.cepilek@gmail.com>
2024-06-19 15:21:31 +0200

    add link to file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
+import { getUrl } from 'aws-amplify/storage';
 
 import Todos from './components/Todos.vue';
+
+const linkToStorageFile = await getUrl({
+  path: 'UNILED_katalog_2024.pdf',
+  options: {
+    validateObjectExistence: false,
+    expiresIn: 20,
+    useAccelerateEndpoint: true,
+  },
+});
+console.log('signed URL: ', linkToStorageFile.url);
+console.log('URL expires at: ', linkToStorageFile.expiresAt);
 
 </script>
 
@@ -13,6 +25,11 @@ import Todos from './components/Todos.vue';
         <h1>Hell {{ user?.signInDetails?.loId }}'s todos'</h1>
         <Todos />
         <button @click="signOut">Sign Out</button>
+        <div>
+          <a href={linkToStorageFile.url.toString()} target="_blank" rel="noreferrer">
+            {filename}
+          </a>
+        </div>
       </template>
     </authenticator>
   </main>
